5cdee97213c80e556f53e67ff243034627871f2a
juxingmaomi <maomi610648174@gmail.com>
2026-07-12 10:56:07 +0800

    Update tavern-helper-loader.js for v0.5.1

--- tavern-helper-loader.js
+++ tavern-helper-loader.js
@@ -1,6 +1,6 @@
 ﻿(async () => {
   const REPO = 'juxingmaomi/message-star-marker';
-  const VERSION = 'v0.5.0';
+  const VERSION = 'v0.5.1';
   const URL = `https://cdn.jsdelivr.net/gh/${REPO}@${VERSION}/index.js`;
 
   const loaderState = {
