6cdb8d11bacb7e57b4f081922ba37a32df155143
juxingmaomi <maomi610648174@gmail.com>
2026-07-15 06:46:19 +0800

    Release reader-v0.2.0: customizable marker settings

--- tavern-helper-reader-loader.juxingmaomi.js
+++ tavern-helper-reader-loader.juxingmaomi.js
@@ -1,6 +1,6 @@
 (async () => {
   const REPO = 'juxingmaomi/message-star-marker';
-  const VERSION = 'reader-v0.1.9';
+  const VERSION = 'reader-v0.2.0';
   const URL = `https://cdn.jsdelivr.net/gh/${REPO}@${VERSION}/reader.js`;
 
   const loaderState = {
